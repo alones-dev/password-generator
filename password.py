ad89e7bc5a29658b26083a48fba67b77812d7a00
Alones <79063304+alones-dev@users.noreply.github.com>
2021-09-07 21:56:38 +0200

    Update password.py

--- password.py
+++ password.py
@@ -3,6 +3,19 @@ import random
 caracters = ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "q", "s", "d", "f", "g", "h", "j", "k", "l", "m", "w", "x", "c", "v", "b", "n", "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M", "W", "X", "C", "V", "B", "N", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 def generate_password(amount):
+    '''
+    This function allows you to randomly generate a password with a chosen number of characters.
+    :type:
+        amount -> int
+    
+    :return:
+        str
+    
+    :example:
+        generate_password(32)
+        >>> VvVOS52DpqQDGn1BUQjB87q5vkiY8NNo
+    '''
+    assert type(amount) == int, "You must use an integer"
     count = 0
     lstAlt = []
     while count < amount:
